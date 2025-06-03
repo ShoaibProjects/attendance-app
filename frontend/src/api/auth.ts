88364cf434a735a068c60d44b39e0598aaad5d30
ShoaibProjects <shoaibakhtarcs@gmail.com>
2025-06-03 16:27:53 +0530

    more fixes

--- frontend/src/api/auth.ts
+++ frontend/src/api/auth.ts
@@ -2,7 +2,7 @@
 import axios from './axiosInstance';
 
 export const registerUser = (data: { name: string; email: string; password: string }) =>
-  axios.post('/users/register', data);
+  axios.post('/api/users/register', data);
 
 export const loginUser = (data: { email: string; password: string }) =>
-  axios.post('/users/login', data);
+  axios.post('/api/users/login', data);
